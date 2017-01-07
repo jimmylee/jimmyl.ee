77848d0cd6a29c084cd36c634800d14bfe472ea1
Jimmy Lee <jimmy-lee@live.com>
2017-01-07 12:18:37 -0800

    Minifies HTML for additional savings

--- gulp/tasks/copy-index-html-to-build-directory.js
+++ gulp/tasks/copy-index-html-to-build-directory.js
@@ -2,8 +2,18 @@
 
 import gulp from 'gulp';
 import config from '../config';
+import htmlmin from 'gulp-htmlmin';
 
 gulp.task('copy-index-html-to-build-directory', function() {
+  if (!global.isProduction) {
+    gulp.src(config.sourceDirectory + 'index.html')
+      .pipe(gulp.dest(config.buildDirectory));
+    return;
+  }
+
   gulp.src(config.sourceDirectory + 'index.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
     .pipe(gulp.dest(config.buildDirectory));
 });
