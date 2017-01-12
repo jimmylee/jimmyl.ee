1381de298b2f1ab7194d42ac2fa264b7d013ef77
Jimmy Lee <jimmy-lee@live.com>
2017-01-11 21:41:34 -0800

    I don't know, might work

--- gulp/tasks/minify-and-copy-images-to-build-directory.js
+++ gulp/tasks/minify-and-copy-images-to-build-directory.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import gulp from 'gulp';
+import gulpIf from 'gulp-if';
+import gzip from 'gulp-gzip';
 import gulpif from 'gulp-if';
 import imagemin from 'gulp-imagemin';
 import browserSync from 'browser-sync';
@@ -9,6 +11,9 @@ import config from '../config';
 gulp.task('minify-and-copy-images-to-build-directory', function() {
   return gulp.src(config.images.src)
     .pipe(gulpif(global.isProduction, imagemin()))
+    .pipe(gulpIf(global.isProduction, gzip({
+      append: false
+    })))
     .pipe(gulp.dest(config.images.dest))
     .pipe(gulpif(browserSync.active, browserSync.reload({
       stream: true,
